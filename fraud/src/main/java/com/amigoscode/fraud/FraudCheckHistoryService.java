ed04cededef38dc74e145ceaf610ea18006a7d0e
Hassane <o22008958@univ-orleans.fr>
2022-01-31 13:57:19 +0100

    fraud-services

---
+++ fraud/src/main/java/com/amigoscode/fraud/FraudCheckHistoryService.java
@@ -0,0 +1,22 @@
+package com.amigoscode.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class FraudCheckHistoryService {
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public boolean isFraudulentCustomer(Integer customerId){
+        fraudCheckHistoryRepository.save(FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .createdAt(LocalDateTime.now())
+                        .isFraudster(false)
+                        .build());
+        return false;
+    }
+}
