3b8d64fd501f8c0d03515be228c857ea0fa8e542
Barry Hassane <hassane.barry@etu.univ-orleans.fr>
2022-02-17 12:36:43 +0100

    rabbitMQ producer and consumer

--- notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -1,13 +1,28 @@
 package com.amigoscode.notification;
 
+import com.amigoscode.amqp.RabbitMQMesageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.amigoscode.notification",
+                "com.amigoscode.amqp"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+    //@Bean
+    //CommandLineRunner commandLineRunner(RabbitMQMesageProducer producer, NotificationConfig notificationConfig){
+      //  return args -> {
+        //    producer.publish("foo", notificationConfig.getInternalExchange(), notificationConfig.getInternalNotificationRoutingKey());
+       // };
+   // }
 }
